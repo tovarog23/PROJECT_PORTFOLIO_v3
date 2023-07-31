6a9f013a1e6ec30701e8d3173274bdbd40bd3eda
Roger <tovarog23@gmail.com>
2023-07-31 13:49:46 -0600

    Add media queries to index and about me

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -2,7 +2,7 @@
 
 let welcome_phrase = document.getElementById("greetings");
 
-const helloList = ["Hello","Bonjour","Hola","Zdravstvuyte","Nǐn hǎo","Salve","Konnichiwa","Guten Tag","Olá","Anyoung haseyo","Asalaam alaikum","Goddag","Shikamoo","Goedendag","Yassas","Dzień dobry","Selamat siang","God dag","Merhaba","Shalom","God dag"];
+const helloList = ["Hello","Konnichiwa","Hola","Kia Ora","Nǐn hǎo","Salve","Konnichiwa","Guten Tag","Olá","Goddag","Shikamoo","Goedendag","Yassas","Merhaba","Shalom"];
 
 let currentTextIndex = 0;
 console.log(helloList.length);
